cdb54e5d5c1e909a61e7cc4729d864d1c2d756d6
Bojan Negovanovic <stracatone@gmail.com>
2024-01-11 14:47:17 +0100

    SMS listener added

--- app/src/main/java/com/example/jetpacktestapp/compose/home/HomeScreen.kt
+++ app/src/main/java/com/example/jetpacktestapp/compose/home/HomeScreen.kt
@@ -6,6 +6,8 @@ import android.content.Context.RECEIVER_EXPORTED
 import android.content.Intent
 import android.content.IntentFilter
 import android.os.Build
+import android.provider.CalendarContract
+import android.telephony.SmsMessage
 import android.telephony.TelephonyManager
 import android.util.Log
 import androidx.annotation.RequiresApi
@@ -17,31 +19,40 @@ import androidx.compose.ui.platform.LocalContext
 @Composable
 fun HomeScreen(onItemClick: (String) -> Unit = {}) {
     val filter = IntentFilter()
-    filter.addAction(TelephonyManager.ACTION_PHONE_STATE_CHANGED)
+    filter.addAction("android.provider.Telephony.SMS_RECEIVED")
     val receiver = UnlockReceiver()
+    CalendarContract.Events.
     LocalContext.current.registerReceiver(receiver, filter, RECEIVER_EXPORTED)
 }
 
 
 class UnlockReceiver : BroadcastReceiver() {
     override fun onReceive(context: Context, intent: Intent) {
-        if (intent.action != null && intent.action.equals(TelephonyManager.ACTION_PHONE_STATE_CHANGED)) {
+        if (intent.action != null && intent.action.equals("android.provider.Telephony.SMS_RECEIVED")) {
             Log.d("+++", "Data extra: ${intent.getStringExtra(TelephonyManager.EXTRA_STATE)}")
             // Device unlocked, perform actions here
             // For example, start a service or launch an activity
-            when(intent.getStringExtra(TelephonyManager.EXTRA_STATE)) {
-                TelephonyManager.EXTRA_STATE_RINGING -> {
+            val bundle = intent.extras
+            if (bundle != null) {
+                // Extract the SMS messages
+                val pdus = bundle["pdus"] as Array<Any>?
+                if (pdus != null) {
+                    val messageCount = pdus.size
+//                    Log.d(TAG, "Number of incoming messages: $messageCount")
 
-                }
-                TelephonyManager.EXTRA_STATE_OFFHOOK -> {
+                    // You can handle each message individually if needed
+                    for (pdu in pdus) {
+                        val smsMessage: SmsMessage = SmsMessage.createFromPdu(pdu as ByteArray)
+                        val messageBody: String = smsMessage.getMessageBody()
+                        val senderPhoneNumber: String = smsMessage.getOriginatingAddress() ?: ""
 
-                }
-                TelephonyManager.EXTRA_STATE_IDLE -> {
-
+                        // Process the message as needed
+                        Log.d("+++", "Sender: $senderPhoneNumber, Message: $messageBody")
+                    }
+                    Log.d("+++", "msg count: $messageCount")
                 }
             }
 
-            Log.d("+++", "Phone unlocked")
         }
     }
 }
